89d980fe97119ad45c97e5fdf39c7b10a1deeda6
ilyass shw <souwain.malak@gmail.com>
2020-12-16 15:11:52 +0100

    make random test

--- file.js
+++ file.js
@@ -1,1 +1,6 @@
-console.log('gg');
+const randWords ={
+    verbs: ['start', 'work on', 'finish'],
+    projectNames: [' a serious project', 'a fun project', 'a long project'] 
+};
+
+console.log('I am going to '+randWords.verbs[Math.floor(Math.random()*randWords.verbs.length)]+' '+randWords.projectNames[Math.floor(Math.random()*randWords.projectNames.length)]+'.')
